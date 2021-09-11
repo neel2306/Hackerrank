ccdda475714b45da4e76e3d11b9c1fda97f067d2
Neelabh Somani <83975955+SedHabibi@users.noreply.github.com>
2021-09-11 20:13:51 +0530

    Create Day_4.py

---
+++ 30-Days-of-Code-Python/Day_4.py
@@ -0,0 +1,27 @@
+'''
+TASK:
+Write a Person class with an instance variable, , and a constructor that takes an integer, , as a parameter. The constructor must assign  to  after confirming the argument passed as  is not negative; if a negative argument is passed as , the constructor should set  to  and print Age is not valid, setting age to 0.. In addition, you must write the following instance methods:
+
+yearPasses() should increase the  instance variable by .
+amIOld() should perform the following conditional actions:
+If , print You are young..
+If  and , print You are a teenager..
+Otherwise, print You are old..
+To help you learn by example and complete this challenge, much of the code is provided for you, but you'll be writing everything in the future. The code that creates each instance of your Person class is in the main method. Don't worry if you don't understand it all quite yet!
+'''
+class Person:
+    def __init__(self,initialAge):
+        if initialAge < 0:
+            self.age = 0
+            print("Age is not valid, setting age to 0.")
+        elif initialAge >= 0:
+            self.age = initialAge
+    def amIOld(self):
+        if self.age < 13:
+            print("You are young.")
+        elif self.age >= 13 and self.age < 18:
+            print("You are a teenager.")
+        else:
+            print("You are old.")
+    def yearPasses(self):
+        self.age+=1
